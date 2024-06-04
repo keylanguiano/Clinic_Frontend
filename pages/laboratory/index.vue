11c3c6c4fc2c00211e6517be865a14b9f00d91dd
keylanguiano <keyla_anguiano@hotmail.com>
2024-06-03 18:22:45 -0600

    Funcionalidad de tablas y progress circular mediante información de la bd

--- pages/laboratory/index.vue
+++ pages/laboratory/index.vue
@@ -26,13 +26,13 @@
 
                 <v-col cols="3" class="ma-0 pa-0 d-flex flex-column" style="height: 293px !important;">
                     <v-row class="ma-0 pa-0 align-center justify-center">
-                        <user-progress-circular :percentage="60" :data="'142'" :color="'#874C96'" :topic="'In Progress'" />
+                        <user-progress-circular :percentage="calculatePercentage(items_total_testing, 'In-Progress')" :data="inProgressCountTesting" :color="'#874C96'" :topic="'In Progress'" />
                     </v-row>
 
                     <v-spacer />
 
                     <v-row class="ma-0 pa-0 align-center justify-center">
-                        <user-progress-circular :percentage="90" :data="'562'" :color="'#ffccba'" :topic="'Done'" />
+                        <user-progress-circular :percentage="calculatePercentage(items_total_testing, 'Completed')" :data="completedCountTesting" :color="'#ffccba'" :topic="'Done'" />
                     </v-row>
                 </v-col>
             </v-row>
@@ -60,13 +60,13 @@
 
                 <v-col cols="3" class="ma-0 pa-0 d-flex flex-column" style="height: 293px !important;">
                     <v-row class="ma-0 pa-0 align-center justify-center">
-                        <user-progress-circular :percentage="60" :data="'142'" :color="'#874C96'" :topic="'not issued'" />
+                        <user-progress-circular :percentage="calculatePercentage(items_total_result, 'In-Progress')" :data="inProgressCountResult" :color="'#874C96'" :topic="'not issued'" />
                     </v-row>
 
                     <v-spacer />
 
                     <v-row class="ma-0 pa-0 align-center justify-center">
-                        <user-progress-circular :percentage="90" :data="'562'" :color="'#ffccba'" :topic="'issued'" />
+                        <user-progress-circular :percentage="calculatePercentage(items_total_result, 'Completed')" :data="completedCountResult" :color="'#ffccba'" :topic="'issued'" />
                     </v-row>
                 </v-col>
             </v-row>
@@ -93,34 +93,183 @@ export default {
     auth: true,
     data () {
         return {
+            token: '',
+            doctor: '',
             headers: [
-                { text: 'Name', value: 'name', align: 'center', sortable: true },
+                { text: 'Name', value: 'patient', align: 'center', sortable: true },
                 { text: 'Date', value: 'date', align: 'center', sortable: false },
                 { text: 'Admin', value: 'admin', align: 'center', sortable: false },
                 { text: 'Status', value: 'status', align: 'center', sortable: false },
                 { text: 'Time Taken', value: 'time_taken', align: 'center', sortable: false }
             ],
-            items_total_testing: [
-                { name: 'Mathews', date: '01/01/2023', admin: 'samnatha', status: 'in-progress', time_taken: '2 hrs' },
-                { name: 'Swetha Menon', date: '07/01/2023', admin: 'bobP', status: 'in-progress', time_taken: '1.25 hrs' },
-                { name: 'Jack Ma', date: '24/01/2023', admin: 'kate.F', status: 'completed', time_taken: '20 mins' },
-                { name: 'Jack', date: '27/01/2023', admin: 'jack O', status: 'in-progress', time_taken: '60 mins' }
-            ],
-            items_total_result: [
-                { name: 'antony', date: '01/01/2023', admin: 'samnatha', status: 'completed', time_taken: '2 hrs' },
-                { name: 'Divya', date: '07/01/2023', admin: 'bobP', status: 'completed', time_taken: '1.25 hrs' },
-                { name: 'manish', date: '24/01/2023', admin: 'kate.F', status: 'completed', time_taken: '20 mins' },
-                { name: 'kanam', date: '27/01/2023', admin: 'jack O', status: 'completed', time_taken: '60 mins' }
-            ]
+            items_total_testing: [],
+            items_total_result: [],
+            doctors: [],
+            patients: [],
+            admins: [],
+            tests: [],
+            inProgressCountTesting: 0,
+            completedCountTesting: 0,
+            inProgressCountResult: 0,
+            completedCountResult: 0
         }
+    },
+    mounted () {
+        this.token = localStorage.getItem('Token')
+        this.doctor = localStorage.getItem('Doctor')
+
+        console.log('token', this.token)
+        console.log('doctor', this.doctor)
+
+        this.getPatients()
+        this.getDoctors()
+        this.getAdmins()
+        this.getTest()
     },
     methods: {
         statusClass (status) {
             return {
-                'laboratory-table-status-completed': status === 'completed',
-                'laboratory-table-status-done': status === 'done',
-                'laboratory-table-status-in-progress': status === 'in-progress'
+                'laboratory-table-status-completed': status === 'Completed',
+                'laboratory-table-status-done': status === 'Done',
+                'laboratory-table-status-in-progress': status === 'In-Progress'
             }
+        },
+        async getPatients () {
+            const url = '/get-all-patients'
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${this.token}`
+                }
+            }
+
+            this.$axios.get(url, config)
+                .then((res) => {
+                    console.log('@ Keyla => Response ', res)
+
+                    if (res.data.message === 'Success') {
+                        this.patients = res.data.patients
+
+                        console.log('@ Keyla => Patients ', this.patients)
+                    }
+                })
+                .catch((err) => {
+                    console.log('@ Keyla => Error Frontend', err)
+                })
+        },
+        async getDoctors () {
+            const url = '/get-all-doctors'
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${this.token}`
+                }
+            }
+
+            this.$axios.get(url, config)
+                .then((res) => {
+                    console.log('@ Keyla => Response ', res)
+
+                    if (res.data.message === 'Success') {
+                        this.doctors = res.data.doctors
+
+                        console.log('@ Keyla => Doctors ', this.doctors)
+                    }
+                })
+                .catch((err) => {
+                    console.log('@ Keyla => Error Frontend', err)
+                })
+        },
+        async getAdmins () {
+            const url = '/get-all-admins'
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${this.token}`
+                }
+            }
+
+            this.$axios.get(url, config)
+                .then((res) => {
+                    console.log('@ Keyla => Response ', res)
+
+                    if (res.data.message === 'Success') {
+                        this.admins = res.data.admins
+
+                        console.log('@ Keyla => Admins ', this.admins)
+                    }
+                })
+                .catch((err) => {
+                    console.log('@ Keyla => Error Frontend', err)
+                })
+        },
+        async getTest () {
+            const url = '/get-all-tests'
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${this.token}`
+                }
+            }
+
+            this.$axios.get(url, config)
+                .then((res) => {
+                    console.log('@ Keyla => Response ', res)
+
+                    if (res.data.message === 'Success') {
+                        this.tests = res.data.tests
+
+                        console.log('@ Keyla => Tests ', this.tests)
+
+                        this.items_total_testing = this.tests
+                            .filter(test => test.result === null && test.email_doctor === this.doctor)
+                            .map(test => {
+                                const patient = this.patients.find(p => p.email === test.email_patient)
+                                const doctor = this.doctors.find(d => d.email === test.email_doctor)
+                                const admin = this.admins.find(a => a.email === test.email_admin)
+
+                                return {
+                                    ...test,
+                                    patient: patient.name,
+                                    doctor: doctor.name,
+                                    admin: admin.name
+                                }
+                            })
+
+                        console.log('@ Keyla => items_total_testing ', this.items_total_testing)
+
+                        this.items_total_result = this.tests
+                            .filter(test => test.result !== null && test.email_doctor === this.doctor)
+                            .map(test => {
+                                const patient = this.patients.find(p => p.email === test.email_patient)
+                                const doctor = this.doctors.find(d => d.email === test.email_doctor)
+                                const admin = this.admins.find(a => a.email === test.email_admin)
+
+                                return {
+                                    ...test,
+                                    patient: patient.name,
+                                    doctor: doctor.name,
+                                    admin: admin.name
+                                }
+                            })
+
+                        console.log('@ Keyla => items_total_result ', this.items_total_result)
+
+                        this.inProgressCountTesting = this.countItemsByStatus(this.items_total_testing, 'In-Progress')
+                        this.completedCountTesting = this.countItemsByStatus(this.items_total_testing, 'Completed')
+                        this.inProgressCountResult = this.countItemsByStatus(this.items_total_result, 'In-Progress')
+                        this.completedCountResult = this.countItemsByStatus(this.items_total_result, 'Completed')
+                    }
+                })
+                .catch((err) => {
+                    console.log('@ Keyla => Error Frontend', err)
+                })
+        },
+        calculatePercentage (items, status) {
+            const itemCount = this.countItemsByStatus(items, status)
+            if (items.length === 0) {
+                return 0
+            }
+            return (itemCount / items.length) * 100
+        },
+        countItemsByStatus (items, status) {
+            return items.filter(item => item.status === status).length
         }
     }
 }
